516bd58076980be583629c124e95d86c24d2131c
stevecyj <ycsteven.tsao@gmail.com>
2023-02-01 17:31:08 +0800

    composition methods

---
+++ src/pages/ComCounter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>{{ count }}</div>
+  <button @click="add">add</button>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  setup() {
+    let count = ref(0);
+
+    function add() {
+      count.value++;
+    }
+
+    return {
+      count,
+      add,
+    };
+  },
+});
+</script>
+
+<style></style>
