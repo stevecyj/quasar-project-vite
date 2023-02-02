5ab09d7ea641a73b1087b9e718dfa572197cf010
stevecyj <ycsteven.tsao@gmail.com>
2023-02-02 14:22:39 +0800

    setup syntactic sugar

---
+++ src/pages/SetupCounter.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>{{ count }}</div>
+  <div class="color">{{ doubleCount }}</div>
+  <button @click="add">add</button>
+</template>
+
+<script setup>
+import { computed, ref, watch } from 'vue';
+
+let count = ref(0);
+let color = ref('red');
+
+function add() {
+  count.value++;
+}
+
+watch(count, () => {
+  console.log('sss');
+  if (count.value % 2 === 0) {
+    color.value = 'red';
+  } else {
+    color.value = 'green';
+  }
+});
+
+let doubleCount = computed(() => {
+  return count.value * 2;
+});
+</script>
+
+<style>
+.color {
+  color: v-bind(color);
+}
+</style>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -5,7 +5,7 @@ const routes: RouteRecordRaw[] = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     // children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
-    children: [{ path: '', component: () => import('pages/ComCounter.vue') }],
+    children: [{ path: '', component: () => import('pages/SetupCounter.vue') }],
   },
 
   // Always leave this as last one,
