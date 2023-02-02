05cbb6c327e30744d87eed5805b17bee050841cf
stevecyj <ycsteven.tsao@gmail.com>
2023-02-02 15:18:15 +0800

    dark mode

--- src/pages/StyleIdent.vue
+++ src/pages/StyleIdent.vue
@@ -1,4 +1,21 @@
 <template>
   <div class="box text-primary q-ma-sm-sm q-ma-md-xl">lorem</div>
   <div>JOJO</div>
+  <button @click="toggleDark">toggle dark mode</button>
 </template>
+
+<script setup>
+import { useQuasar } from 'quasar';
+
+const $q = useQuasar();
+
+const toggleDark = () => {
+  $q.dark.toggle();
+};
+</script>
+
+<style scoped>
+body.body--dark {
+  background: darkgray;
+}
+</style>
